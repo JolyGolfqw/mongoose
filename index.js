cb8abdb6acb1cd45331d0b47ee237d7a3c43cc06
Rasul <76661035+JolyGolfqw@users.noreply.github.com>
2022-09-09 15:46:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use(require("./routers/routers"));
+
+const port = 3000;
+
 mongoose
   .connect("mongodb+srv://Mansur:1954@cluster0.xyb0huh.mongodb.net/test", {
     useNewUrlParser: true,
@@ -7,12 +15,6 @@ mongoose
   .then(() => console.log("Успешно соединились с сервером MongoDB"))
   .catch(() => console.log("Ошибка при соединении с сервером MongoDB"));
 
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.use(express.json());
-app.use(require("./routers/routers"));
 
 app.listen(port, function (req, res) {
   console.log("Сервер запущен.");
